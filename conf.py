6b4203296b3b6b62e1bb29ec75ea181d56c7611e
Simon Frei <freisim93@gmail.com>
2017-01-23 14:22:53 +0100

    config.py: Revert copyright year to actual original value (2015)

--- conf.py
+++ conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Syncthing'
-copyright = u'2016, The Syncthing Authors'
+copyright = u'2015, The Syncthing Authors'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
