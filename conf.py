a5ca0a2c16a7ec88af8e88696cef40954937a125
Jakob Borg <jakob@kastelo.net>
2018-01-31 20:09:57 +0100

    conf: Minor latex tweaks

--- conf.py
+++ conf.py
@@ -206,10 +206,10 @@ html_context = {
 
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    'papersize': 'a4paper',
 
 # The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    'pointsize': '11pt',
 
 # Additional stuff for the LaTeX preamble.
 #'preamble': '',
