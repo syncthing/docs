6a01116e019f32fb2d3de3a7935b8356b745b80d
Jakob Borg <jakob@kastelo.net>
2020-05-12 09:42:01 +0200

    thanks: With nicknames

---
+++ refresh-authors.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -euo pipefail
+
+# "git contributors" is https://github.com/calmh/git-contributors
+
+git contributors --read-authors=AUTHORS --authors > authors-new
+grep \# AUTHORS > authors-hdr
+cat authors-hdr authors-new > AUTHORS
+rm authors-hdr authors-new
+
+git contributors --read-authors=AUTHORS --names --geekrank > thanks.txt
+
