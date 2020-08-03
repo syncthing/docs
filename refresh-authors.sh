c9bce0216aaa1e0014c8ea4564fadf997aca8504
Tomasz Wilczyński <twilczynski@naver.com>
2020-08-03 16:32:09 +0900

    wip
    Signed-off-by: Tomasz Wilczyński <twilczynski@naver.com>

--- refresh-authors.sh
+++ refresh-authors.sh
@@ -8,7 +8,4 @@ grep \# AUTHORS > authors-hdr
 cat authors-hdr authors-new > AUTHORS
 rm authors-hdr authors-new
 
-git contributors --read-authors=AUTHORS --names --geekrank > thanks.txt
-
-# Underscores need be escaped in RST.
-sed -i '/_/ s//\\_/g' thanks.txt
+git contributors --read-authors=AUTHORS --names --geekrank | sed 's/_/\\_/g' > thanks.txt
