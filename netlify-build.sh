0aa4ac6cc21d7f4edc9823fc38ecb20abba6032c
Jakob Borg <jakob@kastelo.net>
2022-01-12 21:53:26 +0100

    Generate a versions.json (ref syncthing/syncthing#8107) (#711)

--- netlify-build.sh
+++ netlify-build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
-# Download pre-rendered / old versions of docs if this is a production (main
-# branch) build.
-if [[ ${CONTEXT:-} == "production" ]]; then
-    mkdir -p _build
-    git clone https://github.com/syncthing/docs-pre-rendered.git _build/html
-    rm -rf _build/html/.git
-fi
+# Download pre-rendered / old versions of docs.
+mkdir -p _build
+git clone https://github.com/syncthing/docs-pre-rendered.git _build/html
+rm -rf _build/html/.git
+go run _script/lsver.go _build/html > _build/html/versions.json
 
+# Build the current set of docs.
 make html
 make man
 mv _build/man _build/html
