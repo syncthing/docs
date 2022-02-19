d3384cb5c9f192882a2d69957da83fbf7a7bb658
André Colomb <src@andre.colomb.de>
2022-02-19 23:38:14 +0100

    Revert "TESTING ONLY! Switch to rebuilt historic versions repo."
    This reverts commit 234c3df383d2d05c3eb219729e831eb929914b59.

--- netlify-build.sh
+++ netlify-build.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 # Download pre-rendered / old versions of docs.
 mkdir -p _build
-git clone --depth 1 -b inject-version-picker https://github.com/acolomb/syncthing-docs-pre-rendered.git _build/html
+git clone --depth 1 https://github.com/syncthing/docs-pre-rendered.git _build/html
 rm -rf _build/html/.git
 go run _script/lsver.go _build/html > _build/html/versions.json
 
