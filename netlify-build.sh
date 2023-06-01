4809412e4f8c39083834432073c51a6cd4258caf
Jakob Borg <jakob@kastelo.net>
2023-06-01 13:53:22 +0200

    wip

--- netlify-build.sh
+++ netlify-build.sh
@@ -6,11 +6,11 @@ git fetch --tags https://github.com/syncthing/docs.git
 
 # Download pre-rendered / old versions of docs.
 mkdir -p _build
-git clone --depth 1 https://github.com/syncthing/docs-pre-rendered.git _build/html
-rm -rf _build/html/.git
+rclone sync spaces:syncthing-docs-rendered/ _build/html/
 go run _script/lsver.go _build/html > _build/html/versions.json
 
 # Build the current set of docs.
 make html
 make man
 mv _build/man _build/html
+
