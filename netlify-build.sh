9cdd522e7e5057797819d78baa5c397322068e87
Jakob Borg <jakob@kastelo.net>
2023-06-01 14:04:26 +0200

    wip

--- netlify-build.sh
+++ netlify-build.sh
@@ -4,10 +4,17 @@ set -euo pipefail
 # Fetch tags since Netlify doesn't
 git fetch --tags https://github.com/syncthing/docs.git
 
+# Install RClone
+mkdir -p ~/bin
+pushd ~/bin
+curl -O https://downloads.rclone.org/rclone-current-linux-amd64.zip
+unzip rclone-current-linux-amd64.zip
+mv rclone-*-linux-amd64/rclone .
+popd
+
 # Download pre-rendered / old versions of docs.
 mkdir -p _build
-curl https://rclone.org/install.sh | bash
-rclone sync spaces:syncthing-docs-rendered/ _build/html/
+~/bin/rclone sync spaces:syncthing-docs-rendered/ _build/html/
 go run _script/lsver.go _build/html > _build/html/versions.json
 
 # Build the current set of docs.
