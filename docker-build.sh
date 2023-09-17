0a81cfd24776f54003ab16a13dc8a7420b4b462d
Jakob Borg <jakob@kastelo.net>
2023-09-17 16:56:12 +0200

    Use official Sphinx image

--- docker-build.sh
+++ docker-build.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+git describe --tags --long --always > RELEASE || true
+git describe --tags --exact-match > TAG || true
+
 target="${1:-html}"
 docker run --rm \
   -v "$(pwd):$(pwd)" \
   -w "$(pwd)" \
-  ghcr.io/kastelo/sphinx-build:latest \
+  docker.io/sphinxdoc/sphinx-latexpdf:latest \
   make "$target"
