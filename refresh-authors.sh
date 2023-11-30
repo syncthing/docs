c184c3626bcb8801a3225b1dbf052d9ecc729315
Jakob Borg <jakob@kastelo.net>
2023-11-30 22:07:35 +0100

    wip

--- refresh-authors.sh
+++ refresh-authors.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -euo pipefail
+set -x
 
-# "git contributors" is https://github.com/calmh/git-contributors
-
+go install github.com/calmh/git-contributors@latest
 git-contributors --read-authors=AUTHORS --authors > authors-new
 grep \# AUTHORS > authors-hdr
 cat authors-hdr authors-new > AUTHORS
